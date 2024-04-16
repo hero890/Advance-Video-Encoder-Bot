6d52c2c0a1241d0fcd89b5c256d47c99f19f5c09
Tech HEro <159156837+hero890@users.noreply.github.com>
2024-04-16 20:29:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -34,7 +34,7 @@ Send me the correct ffmpeg code for more info.
 
 <code> -c:v libx264 -crf 23 </code> Tʜɪs ɪs ʏᴏᴜʀ ғғᴍᴘᴇɢ ᴄᴏᴅᴇ ✅
 
-📥 Fᴏʀ Hᴇʟᴘ[Cᴏɴᴛ.](https://t.me/+6LwHBLWZc3IyMTU1) 
+📥 Fᴏʀ Hᴇʟᴘ[Cᴏɴᴛ](https://t.me/+6LwHBLWZc3IyMTU1) 
 """
 
     SEND_METADATA ="""
@@ -44,7 +44,7 @@ Send me the correct ffmpeg code for more info.
 
 ◦ <code> -map 0 -c:s copy -c:a copy -c:v copy -metadata title="My Video" -metadata author="John Doe" -metadata:s:s title="Subtitle Title" -metadata:s:a title="Audio Title" -metadata:s:v title="Video Title" </code>
 
-📥 Fᴏʀ Hᴇʟᴘ [Cᴏɴᴛ.](https://t.me/+6LwHBLWZc3IyMTU1) 
+📥 Fᴏʀ Hᴇʟᴘ [Cᴏɴᴛ](https://t.me/+6LwHBLWZc3IyMTU1) 
 """
 
     
@@ -62,7 +62,7 @@ Available commands:-
 ➜ To Set Thumbnail just send photo
 
 
-<b>⦿ Developer:</b> <a href=https://t.me/+6LwHBLWZc3IyMTU1> ꧁༺ 𝓣𝓔𝓒𝓗_𝓗𝓔𝓡𝓞 ༻꧂ ❄️</a>
+<b>⦿ Developer:</b> <a href=https://t.me/+6LwHBLWZc3IyMTU1>꧁༺ 𝓣𝓔𝓒𝓗_𝓗𝓔𝓡𝓞 ༻꧂ ❄️</a>
 """
 
     ABOUT_TXT = """<b>╭───────────⍟
